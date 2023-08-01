5dc637c70cdb02d4c320e5e40603f9adc489fba0
JuzJuJazzy <guide2korawit@gmail.com>
2023-08-01 21:55:46 +0700

    traning room

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-print("Hello, World!")
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
-print("yeh yeh my git")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message:', message)
+    response = 'Server: You said - ' + message
+    emit('response_message', response, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
