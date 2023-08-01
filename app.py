8bdbeac5b9cabb9f792d0fce81ae8323381d0dee
JuzJuJazzy <guide2korawit@gmail.com>
2023-08-01 22:59:39 +0700

    traning room

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -12,8 +13,31 @@ def home():
 @socketio.on('message')
 def handle_message(message):
     print('Received message:', message)
-    response = 'Server: You said - ' + message
+    response_gpt = chat_with_gpt3(message)
+    response = 'Server:' + response_gpt
     emit('response_message', response, broadcast=True)
+
+def chat_with_gpt3(prompt):
+    api_key = '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'  # ใส่ API key ที่ได้รับจาก OpenAI ที่นี่
+    url = 'https://api.openai.com/v1/chat/completions'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+    data = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': prompt},
+        ],
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        return response.json()['choices'][0]['message']['content']
+    else:
+        return f"Error: {response.status_code} - {response.json()['error']['message']}"
+
 
 if __name__ == '__main__':
     socketio.run(app)
